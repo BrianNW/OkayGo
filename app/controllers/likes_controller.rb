a8ec9348a3bd877b0ad620c3a8e8c573d3770e94
ambsvan <amberslouise@gmail.com>
2014-07-25 01:36:48 +0000

    commented out destroy method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -18,10 +18,10 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = current_user.likes.delete({:target_id => params[:target_id]})
-    # # @like = Like.find(params[:id])
-    # @like.destroy
-    redirect_to users_path, notice: "Unliked!"
+    # @like = current_user.likes.delete({:target_id => params[:target_id]})
+    # # # @like = Like.find(params[:id])
+    # # @like.destroy
+    # redirect_to users_path, notice: "Unliked!"
   end
 
   # def likes_params
