5443ee805932793a6b63120721c8d15c5c93e3d5
ambsvan <amberslouise@gmail.com>
2014-07-27 22:56:11 +0000

    formatted forms & user login fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
 def create
   user = User.find_by_username(params[:username])
-  if user
+  if user && user.authenticate(params[:password])
     session[:user_id] = user.id
     redirect_to users_path, notice: "Logged in!"
   else
